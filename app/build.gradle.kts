b11be52067813f3d5c4434a1180ae7cdf9ab4b11
eugene <eugene.dbv@gmail.com>
2024-06-12 13:43:32 +0300

    refactor project before step 6

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    application
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.6")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.3")
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
